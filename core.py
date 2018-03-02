8c9653d435714e66d823dfa8070f83cec9bdaf2e
amanagr <f2016561@pilani.bits-pilani.ac.in>
2018-03-03 01:03:04 +0530

    Model: Add mechanism to fetch new messages.
    Use the focused message's id as anchor to load unread messages
    after it.
    Fix IndexError on old streams with no new messages.

--- core.py
+++ core.py
@@ -21,20 +21,36 @@ class ZulipController:
         self.view = ZulipView(self)
 
     def narrow_to_stream(self, button: Any) -> None:
-        self.view.narrow = json.dumps([['stream', button.caption]])
+        self.model.narrow = [['stream', button.caption]]
         self.model.msg_view.clear()
         messages = self.model.messages[button.stream_id]
         if len(messages) == 0:
-            classified_msgs = self.model.load_old_messages(False)
+            self.model.num_after = 10
+            self.model.num_before = 30
+            classified_msgs = self.model.load_old_messages(True)
             messages = classified_msgs[button.stream_id]
+            # FIXME EMPTY `messages` on old streams with no new messages
+            if len(messages) == 0:
+                messages = [{
+                    'content': '  No Messages yet! Why not start the conversation?',
+                    'title': '',
+                    'type': 'stream',
+                    'time': 0,
+                    'sender': '',
+                    'stream': button.caption,
+                    'sender_email': '',
+                    'id' : 10000000000,
+                }]
         self.model.msg_view.extend(create_msg_box_list(messages, self.model))
 
     def narrow_to_user(self, button: Any) -> None:
-        self.view.narrow = json.dumps([["pm_with", button.email]])
+        self.model.narrow = [["pm_with", button.email]]
         self.model.msg_view.clear()
         messages = self.model.messages[button.email]
         if len(messages) == 0:
-            classified_msgs = self.model.load_old_messages(False)
+            self.model.num_after = 10
+            self.model.num_before = 30
+            classified_msgs = self.model.load_old_messages(True)
             messages = classified_msgs[button.email]
             if len(messages) == 0:
                 messages = [{
@@ -50,7 +66,6 @@ class ZulipController:
         self.model.msg_view.extend(create_msg_box_list(messages, self.model))
 
     def show_all_messages(self, button: Any) -> None:
-        self.view.narrow = json.dumps([])
         self.model.msg_view.clear()
         self.model.msg_view.extend(create_msg_box_list(itertools.chain.from_iterable(self.model.messages.values()), self.model))
         self.num_before = len(list(itertools.chain.from_iterable(self.model.messages.values())))
