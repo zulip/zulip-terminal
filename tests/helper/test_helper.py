61dc86e3801dd341f836b83a93fce75b7e894839
amanagr <f2016561@pilani.bits-pilani.ac.in>
2018-03-15 12:38:17 +0530

    Tests: Write tests for `help.update_flag`.
    pytest-mock added to requirements.txt.

--- tests/helper/test_helper.py
+++ tests/helper/test_helper.py
@@ -1,4 +1,7 @@
-from zulipterminal.helper import classify_message
+from zulipterminal.helper import (
+    classify_message,
+    update_flag,
+)
 
 
 def test_classify_message(user_email, messages_successful_response,
@@ -6,3 +9,18 @@ def test_classify_message(user_email, messages_successful_response,
     result = classify_message(user_email,
                               messages_successful_response['messages'])
     assert result == classified_message
+
+
+def test_update_flag(mocker):
+    mock_client = mocker.patch('zulip.Client')
+    mock_api_query = mocker.patch('zulip.Client.do_api_query')
+    update_flag([1, 2], mock_client)
+    mock_api_query.assert_called_once_with(
+        {'flag': 'read', 'messages': [1, 2], 'op': 'add'},
+        '/json/messages/flags',
+        method='POST'
+    )
+
+
+def test_update_flag_empty_msg_list(mocker):
+    assert update_flag([], mocker.patch('zulip.Client')) is None
