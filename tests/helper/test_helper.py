447800ac8ba68edc8958d7f4337a9bfda7d9d1ae
Shivam Gera <gera.shivam@gmail.com>
2018-03-20 14:30:34 +0530

    mypy : Annotate test_helper.py and conftest.py using mypy.

--- tests/helper/test_helper.py
+++ tests/helper/test_helper.py
@@ -2,16 +2,21 @@ from zulipterminal.helper import (
     classify_message,
     update_flag,
 )
+from typing import Any, List, Dict
+
+MSR = Dict[str, List[Dict[str, Any]]]
+CMSG = Dict[Any, List[Dict[str, Any]]]
 
 
-def test_classify_message(user_email, messages_successful_response,
-                          classified_message):
+def test_classify_message(user_email: str,
+                          messages_successful_response: MSR,
+                          classified_message: CMSG) -> None:
     result = classify_message(user_email,
                               messages_successful_response['messages'])
     assert result == classified_message
 
 
-def test_update_flag(mocker):
+def test_update_flag(mocker: Any) -> None:
     mock_client = mocker.patch('zulip.Client')
     mock_api_query = mocker.patch('zulip.Client.do_api_query')
     update_flag([1, 2], mock_client)
@@ -22,5 +27,5 @@ def test_update_flag(mocker):
     )
 
 
-def test_update_flag_empty_msg_list(mocker):
+def test_update_flag_empty_msg_list(mocker: Any) -> None:
     assert update_flag([], mocker.patch('zulip.Client')) is None
