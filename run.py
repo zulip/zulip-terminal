ff0441e3a515d517d782883fd082c4eec574578b
amanagr <f2016561@pilani.bits-pilani.ac.in>
2018-03-22 22:22:10 +0530

    run: Add debug mode; use `-d` to start debug mode.

--- run.py
+++ run.py
@@ -5,6 +5,16 @@ import sys
 from zulipterminal.core import ZulipController
 
 
+def save_stdout():
+    """Save shell screen."""
+    sys.stdout.write("\033[?1049h\033[H")
+
+
+def restore_stdout():
+    """Restore saved shell screen."""
+    sys.stdout.write("\033[?1049l")
+
+
 def parse_args():
     description = '''
         Starts Zulip-Terminal.
@@ -20,18 +30,37 @@ def parse_args():
     parser.add_argument('--theme', '-t',
                         default='default',
                         help='choose color theme. (e.g. blue, light)')
+    # debug mode
+    parser.add_argument("-d",
+                        "--debug",
+                        action="store_true",
+                        help="Start zulip terminal in debug mode.")
 
     args = parser.parse_args()
     return args
 
 
 def main():
+    """
+    Launch Zulip Terminal.
+    """
     args = parse_args()
+    if args.debug:
+        save_stdout()
     try:
         ZulipController(args.config_file, args.theme).main()
-    except KeyboardInterrupt:
+    except Exception:
+        # A unexpected exception occurred, open the debugger in debug mode
+        if args.debug:
+            import pdb
+            pdb.post_mortem()
+    finally:
+        if args.debug:
+            restore_stdout()
+
         print("\nThanks for using the Zulip-Terminal interface.\n")
         sys.exit(1)
+
 
 if __name__ == '__main__':
     main()
