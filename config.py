6d7ca7d4bf38f6c4fc454b3cebb85e76849d77ff
amanagr <f2016561@pilani.bits-pilani.ac.in>
2018-04-10 00:27:19 +0530

    hotkeys: Use KEY_BINDINGS in `config.py` for key presses. Add `G` and `P` hotkeys. Switch between editor and normal mode to allow all key presses while composing messages.

---
+++ config.py
@@ -0,0 +1,24 @@
+KEY_BINDINGS = {
+    'k':
+        ('up', 'scroll up'),
+    'j':
+        ('down', 'scroll down'),
+    'h':
+        ('left', 'Go left'),
+    'l':
+        ('right', 'Go right'),
+    'K':
+        ('page up', 'scroll to top'),
+    'J':
+        ('page down', 'scroll to bottom'),
+    'G':
+        ('end', 'Go to last message in view'),
+}
+
+
+def get_key(key: str) -> str:
+    """
+    Returns the mapped binding for a key if mapped
+    or the key otherwise.
+    """
+    return KEY_BINDINGS.get(key, (key,))[0]
