240834612bac511663274f3d927a79f7c956641e
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2018-11-12 10:47:52 -0800

    tests: config: Add is_command_key tests for [non]matching keys.

--- tests/test_config.py
+++ tests/test_config.py
@@ -19,3 +19,16 @@ def test_keys_for_command(command):
 def test_keys_for_command_invalid_command(command):
     with pytest.raises(config.InvalidCommand):
         config.keys_for_command(command)
+
+
+@pytest.mark.parametrize('command', AVAILABLE_COMMANDS)
+def test_is_command_key_matching_keys(command):
+    for key in config.keys_for_command(command):
+        assert config.is_command_key(command, key)
+
+
+@pytest.mark.parametrize('command', AVAILABLE_COMMANDS)
+def test_is_command_key_nonmatching_keys(command):
+    keys_to_test = USED_KEYS - config.keys_for_command(command)
+    for key in keys_to_test:
+        assert not config.is_command_key(command, key)
