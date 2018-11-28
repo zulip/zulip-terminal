b2c88d6381d98fef0ac7da4277fa050fa1c50a52
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2018-11-28 10:18:30 -0800

    deps: Support python 3.4-3.7 in travis & setup.py.

--- setup.py
+++ setup.py
@@ -59,7 +59,10 @@ setup(
         'License :: OSI Approved :: Apache Software License',
 
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords='',
     packages=find_packages(exclude=['test', 'test.*']),
