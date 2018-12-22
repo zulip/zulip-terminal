dc9def70d4d13fd6c3ed2f9f1c488a2a6c13c3b5
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2018-12-21 20:44:43 -0800

    helper: Require non-Optional Index parameter to index_messages.
    This allows assumption of a valid initial Index, which now occurs in
    Model.__init__; index_messages then becomes simpler.
    
    Tests amended.

--- tests/helper/test_helper.py
+++ tests/helper/test_helper.py
@@ -25,43 +25,51 @@ def test_update_flag_empty_msg_list(mocker: Any) -> None:
 
 def test_index_messages_narrow_all_messages(mocker,
                                             messages_successful_response,
-                                            index_all_messages) -> None:
+                                            index_all_messages,
+                                            initial_index) -> None:
     messages = messages_successful_response['messages']
     model = mocker.patch('zulipterminal.model.Model.__init__',
                          return_value=None)
+    model.index = initial_index
     model.narrow = []
-    assert index_messages(messages, model) == index_all_messages
+    assert index_messages(messages, model, model.index) == index_all_messages
 
 
 def test_index_messages_narrow_stream(mocker,
                                       messages_successful_response,
-                                      index_stream) -> None:
+                                      index_stream,
+                                      initial_index) -> None:
     messages = messages_successful_response['messages']
     model = mocker.patch('zulipterminal.model.Model.__init__',
                          return_value=None)
+    model.index = initial_index
     model.narrow = [['stream', 'PTEST']]
     model.stream_id = 205
-    assert index_messages(messages, model) == index_stream
+    assert index_messages(messages, model, model.index) == index_stream
 
 
 def test_index_messages_narrow_topic(mocker,
                                      messages_successful_response,
-                                     index_topic) -> None:
+                                     index_topic,
+                                     initial_index) -> None:
     messages = messages_successful_response['messages']
     model = mocker.patch('zulipterminal.model.Model.__init__',
                          return_value=None)
+    model.index = initial_index
     model.narrow = [['stream', '7'],
                     ['topic', 'Test']]
     model.stream_id = 205
-    assert index_messages(messages, model) == index_topic
+    assert index_messages(messages, model, model.index) == index_topic
 
 
 def test_index_messages_narrow_user(mocker,
                                     messages_successful_response,
-                                    index_user):
+                                    index_user,
+                                    initial_index) -> None:
     messages = messages_successful_response['messages']
     model = mocker.patch('zulipterminal.model.Model.__init__',
                          return_value=None)
+    model.index = initial_index
     model.narrow = [['pm_with', 'boo@zulip.com']]
     model.user_id = 5140
     model.user_dict = {
@@ -69,15 +77,17 @@ def test_index_messages_narrow_user(mocker,
             'user_id': 5179,
         }
     }
-    assert index_messages(messages, model) == index_user
+    assert index_messages(messages, model, model.index) == index_user
 
 
 def test_index_messages_narrow_user_multiple(mocker,
                                              messages_successful_response,
-                                             index_user_multiple):
+                                             index_user_multiple,
+                                             initial_index) -> None:
     messages = messages_successful_response['messages']
     model = mocker.patch('zulipterminal.model.Model.__init__',
                          return_value=None)
+    model.index = initial_index
     model.narrow = [['pm_with', 'boo@zulip.com, bar@zulip.com']]
     model.user_id = 5140
     model.user_dict = {
@@ -88,7 +98,7 @@ def test_index_messages_narrow_user_multiple(mocker,
             'user_id': 5180
         }
     }
-    assert index_messages(messages, model) == index_user_multiple
+    assert index_messages(messages, model, model.index) == index_user_multiple
 
 
 @pytest.mark.parametrize('msgs_with_stars', [
@@ -99,7 +109,8 @@ def test_index_messages_narrow_user_multiple(mocker,
 def test_index_starred(mocker,
                        messages_successful_response,
                        empty_index,
-                       msgs_with_stars):
+                       msgs_with_stars,
+                       initial_index):
     messages = messages_successful_response['messages']
     for msg in messages:
         if msg['id'] in msgs_with_stars and 'starred' not in msg['flags']:
@@ -107,6 +118,7 @@ def test_index_starred(mocker,
 
     model = mocker.patch('zulipterminal.model.Model.__init__',
                          return_value=None)
+    model.index = initial_index
     model.narrow = [['is', 'starred']]
 
     expected_index = dict(empty_index, all_private={537287, 537288},
@@ -115,4 +127,4 @@ def test_index_starred(mocker,
         if msg_id in msgs_with_stars and 'starred' not in msg['flags']:
             msg['flags'].append('starred')
 
-    assert index_messages(messages, model) == expected_index
+    assert index_messages(messages, model, model.index) == expected_index
