f5ecb2142075b2601ae1762b2f6e622f680ea691
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2019-01-04 13:12:58 -0800

    buttons: Truncate top-left captions if too long & append '..'.
    Amend TopButton text rendering test to account for truncation.

--- zulipterminal/ui_tools/buttons.py
+++ zulipterminal/ui_tools/buttons.py
@@ -20,7 +20,7 @@ class TopButton(urwid.Button):
                  count: int=0) -> None:
         self.caption = caption
         self.count = count
-        self.width = width
+        self.width_for_text_space_count = width - 4
         super().__init__("")
         self._w = self.widget(count)
         self.controller = controller
@@ -31,10 +31,19 @@ class TopButton(urwid.Button):
         self._w = self.widget(count)
 
     def widget(self, count: int) -> Any:
-        count_str = '' if count <= 0 else str(count)
-        spaces = self.width - (3 + len(self.caption) + len(count_str) + 1)
+        count_text = '' if count <= 0 else str(count)
+
+        # Shrink text, but always require at least one space
+        max_caption_length = (self.width_for_text_space_count -
+                              len(str(count_text)) - 1)
+        if len(self.caption) > max_caption_length:
+            caption = self.caption[:max_caption_length-2] + '..'
+        else:
+            caption = self.caption
+        num_spaces = max_caption_length - len(caption) + 1
+
         return urwid.AttrMap(urwid.SelectableIcon(
-            [u' \N{BULLET} ', self.caption, spaces*' ', ('idle',  count_str)],
+            [u' \N{BULLET} ', caption, num_spaces*' ', ('idle',  count_text)],
             0),  # cursor location
             None,
             'selected')
