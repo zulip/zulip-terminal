9ee55f0e8e8ff2e0729e5756ce66d82bb256abe9
aditya-hari <adityahari3791@gmail.com>
2019-02-08 14:10:40 +0530

    Fix quoting original message, not rendered version
    Fixes #239
    Updates CHANGELOG.md

--- zulipterminal/ui_tools/boxes.py
+++ zulipterminal/ui_tools/boxes.py
@@ -472,7 +472,8 @@ class MessageBox(urwid.Pile):
             self.model.controller.view.middle_column.set_focus('footer')
         elif is_command_key('QUOTE_REPLY', key):
             self.keypress(size, 'enter')
-            quote = '```quote\n' + self.message['content'] + '\n```\n'
+            quote = '```quote\n' + self.model.client.get_raw_message(
+                self.message['id'])['raw_content'] + '\n```\n'
             self.model.controller.view.write_box.msg_write_box.set_edit_text(
                 quote)
             self.model.controller.view.write_box.msg_write_box.set_edit_pos(
