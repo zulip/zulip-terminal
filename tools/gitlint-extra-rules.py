1088d2a059473a84ee5dbad18cf66668eb03c49a
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2019-02-27 09:44:51 -0800

    gitlint-extra-rules: Combine area rules into one rule.

--- tools/gitlint-extra-rules.py
+++ tools/gitlint-extra-rules.py
@@ -15,53 +15,36 @@ class EndsWithDot(CommitRule):
             return [RuleViolation(self.id, error, line_nr=1)]
 
 
-class StartsWithAreaColonOrMore(CommitRule):
-    name = "title-missing-area-and-colon"
+class AreaFormatting(CommitRule):
+    name = "area-formatting"
     id = "ZT2"
-
-    def validate(self, commit: Any) -> Optional[List[RuleViolation]]:
-        error = ("Title should start with at least one area "
-                 "followed by a colon and space")
-        title_components = commit.message.title.split(": ")
-
-        if len(title_components) < 2:
-            return [RuleViolation(self.id, error, line_nr=1)]
-
-
-class AreaConstraints(CommitRule):
-    name = "area-constraints"
-    id = "ZT3"
 
     options_spec = [ListOption("exclusions", ["WIP"],
                                "Exclusions to area lower-case rule")]
 
     def validate(self, commit: Any) -> Optional[List[RuleViolation]]:
-        exclusions = self.options['exclusions'].value
-        exclusions_text = ", or ".join(exclusions)
-        if exclusions_text:
-            exclusions_text = " (or {})".format(exclusions_text)
-
-        error = ("Areas at start of title should be lower case{}, "
-                 "followed by ': '".format(exclusions_text))
-
         title_components = commit.message.title.split(": ")
 
         violations = []
 
+        # Return just this violation, since latter checks assume an area
+        error = ("Title should start with at least one area, "
+                 "followed by a colon and space")
+        if len(title_components) < 2:
+            return [RuleViolation(self.id, error, line_nr=1)]
+
+        exclusions = self.options['exclusions'].value
+        exclusions_text = ", or ".join(exclusions)
+        if exclusions_text:
+            exclusions_text = " (or {})".format(exclusions_text)
+        error = ("Areas at start of title should be lower case{}, "
+                 "followed by ': '".format(exclusions_text))
         for area in title_components[:-1]:
             if not (area.islower() or area in exclusions) or ' ' in area:
                 violations += [RuleViolation(self.id, error, line_nr=1)]
 
-        return violations
-
-
-class ChangeCapitalized(CommitRule):
-    name = "capitalized-change"
-    id = "ZT4"
-
-    def validate(self, commit: Any) -> Optional[List[RuleViolation]]:
-        error = "Summary of change, after areas, should be capitalized"
-        title_components = commit.message.title.split(": ")
-
+        error = "Summary of change, after area(s), should be capitalized"
         if not title_components[-1][0].isupper():
-            return [RuleViolation(self.id, error, line_nr=1)]
+            violations += [RuleViolation(self.id, error, line_nr=1)]
+
+        return violations
