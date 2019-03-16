750d444b36bfbceaa5fa5b3734665b1eff999356
Aman Agrawal <f2016561@pilani.bits-pilani.ac.in>
2019-03-16 14:12:48 +0530

    classify_unread_counts: Store unread count of GroupPMs.
    Add fixture for for group_pm in initial_data fixture as `huddles`.
    Add huddles to classify_unread_counts fixture too unreads.

--- tests/helper/test_helper.py
+++ tests/helper/test_helper.py
@@ -163,12 +163,12 @@ def test_powerset(iterable, map_func, expected_powerset):
 
 @pytest.mark.parametrize('muted_streams, muted_topics, vary_in_unreads', [
     ([99], [['Some general stream', 'Some general unread topic']], {
-        'all_msg': 4,
+        'all_msg': 9,
         'streams': {99: 1},
         'unread_topics': {(99, 'Some private unread topic'): 1},
     }),
     ([1000], [['Secret stream', 'Some private unread topic']], {
-        'all_msg': 6,
+        'all_msg': 11,
         'streams': {1000: 3},
         'unread_topics': {(1000, 'Some general unread topic'): 3},
     }),
