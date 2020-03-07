c9c417fa868ecbb140dded4f0d4bfdef4f03baaf
Preet Mishra <ipreetmishra@gmail.com>
2020-03-08 01:15:17 +0530

    tests: ui_tools: Migrate keypress test for return_to_focus_after_search.

--- tests/ui/test_ui_tools.py
+++ tests/ui/test_ui_tools.py
@@ -462,13 +462,17 @@ class TestStreamsView:
         stream_view.set_focus.assert_called_once_with("body")
         assert stream_view.log == self.streams_btn_list
 
+    @pytest.mark.parametrize('search_streams_key',
+                             keys_for_command('SEARCH_STREAMS'))
+    @pytest.mark.parametrize('go_back_key', keys_for_command('GO_BACK'))
     @pytest.mark.parametrize('current_focus, stream', [
         (0, 'FOO'),
         (2, 'fan'),
         (4, 'BOO'),
     ])
     def test_return_to_focus_after_search(self, mocker, stream_view,
-                                          current_focus, stream):
+                                          current_focus, stream,
+                                          search_streams_key, go_back_key):
         # Initialize log
         stream_view.streams_btn_list = [
             mocker.Mock(stream_name=stream_name) for stream_name in [
@@ -482,14 +486,12 @@ class TestStreamsView:
         previous_focus_stream_name = stream
 
         # Toggle Stream Search
-        key = "q"
         size = (20,)
-        stream_view.keypress(size, key)
+        stream_view.keypress(size, search_streams_key)
 
         # Exit Stream Search
-        key = "esc"
         size = (20,)
-        stream_view.keypress(size, key)
+        stream_view.keypress(size, go_back_key)
 
         # Obtain new stream focus
         new_focus = stream_view.log.get_focus()[1]
