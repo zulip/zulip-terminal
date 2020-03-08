a433a302a24644fab373a835998cd118136f737a
Kaustubh Nair <kaustubh.nair108@gmail.com>
2020-03-08 17:43:45 +0530

    model: Handle read events for indexed messages.
    This commit primarily adds handling of updates to the status flags on
    messages for 'read' events, in addition to the previously-present
    'starred' events, in update_message_flag_status.
    
    We previously sent a message to the server indicating a particular
    message had been read locally, also marking it read and updating
    unread-counts locally. Now that we handle the 'read'-flag events, we can
    also handle remotely-read messages, from other clients.
    
    For now, the count updating is moved from the server-message method to
    update_message_flag_status.
    
    NOTE: One current limitation to updating remotely-read messages is that
    we only handle messages we have full content of in the index.
    
    Star-flag update tests amended.
    
    New tests added for updating the read-flag.

--- zulipterminal/model.py
+++ zulipterminal/model.py
@@ -289,7 +289,6 @@ class Model:
             'flag': 'read',
             'op': 'add',
         })
-        set_count(id_list, self.controller, -1)  # FIXME Update?
 
     def send_private_message(self, recipients: str,
                              content: str) -> bool:
@@ -815,9 +814,11 @@ class Model:
         if event['all']:  # FIXME Should handle eventually
             return
 
-        # TODO: Expand from 'starred' to also support 'read' flag changes?
         flag_to_change = event['flag']
-        if flag_to_change != 'starred':
+        if flag_to_change not in {'starred', 'read'}:
+            return
+
+        if flag_to_change == 'read' and event['operation'] == 'remove':
             return
 
         indexed_message_ids = set(self.index['messages'])
@@ -836,6 +837,10 @@ class Model:
 
             self.index['messages'][message_id] = msg
             self.update_rendered_view(message_id)
+
+        if event['operation'] == 'add' and flag_to_change == 'read':
+            set_count(list(message_ids_to_mark & indexed_message_ids),
+                      self.controller, -1)
 
     def update_rendered_view(self, msg_id: int) -> None:
         # Update new content in the rendered view
