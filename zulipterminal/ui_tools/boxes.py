ffccb8802ebfde937db3df327231c3d25770abfc
Preet Mishra <ipreetmishra@gmail.com>
2020-03-12 16:40:42 +0530

    boxes/views: Reset search box text when 'GO_BACK' key is pressed.
    This resets search box text whenever the user presses 'GO_BACK'
    key to clear the search. The text is replaced with its
    appropriate search text along with the key required to trigger
    the search.
    
    Tests amended.
    
    Fixes #536.

--- zulipterminal/ui_tools/boxes.py
+++ zulipterminal/ui_tools/boxes.py
@@ -825,6 +825,9 @@ class PanelSearchBox(urwid.Edit):
         urwid.connect_signal(self, 'change', update_function)
         super().__init__(edit_text=self.search_text)
 
+    def reset_search_text(self) -> None:
+        self.set_edit_text(self.search_text)
+
     def keypress(self, size: urwid_Size, key: str) -> Optional[str]:
         if is_command_key('ENTER', key):
             self.panel_view.view.controller.editor_mode = False
