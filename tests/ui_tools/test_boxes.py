e4fd5796087a618351ce8530389c061a85ceeba4
Kaustubh Nair <kaustubh.nair108@gmail.com>
2020-04-01 14:31:29 +0530

    refactor: boxes: Remove emoji_list argument from autocomplete_emojis.
    Set the value of emoji_list inside autocomplete_emojis to make it more
    consistent with other autocomplete methods.
    
    Tests amended.

--- tests/ui_tools/test_boxes.py
+++ tests/ui_tools/test_boxes.py
@@ -156,8 +156,9 @@ class TestWriteBox:
         (':nomatch', 0, None),
         (':nomatch', -1, None),
         ])
-    def test_autocomplete_emojis(self, write_box, emojis_fixture,
+    def test_autocomplete_emojis(self, write_box, emojis_fixture, mocker,
                                  text, state, required_typeahead):
-        typeahead_string = write_box.autocomplete_emojis(text,
-                                                         state, emojis_fixture)
+        mocker.patch('zulipterminal.ui_tools.boxes.emoji_names',
+                     EMOJI_NAMES=emojis_fixture)
+        typeahead_string = write_box.autocomplete_emojis(text, state)
         assert typeahead_string == required_typeahead
