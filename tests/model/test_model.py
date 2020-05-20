157e397a4c106802816bf87de369ac47586fc41c
Imran Iqbal <iqbalmy@hotmail.com>
2020-05-20 22:39:29 +0100

    feat(model): show desktop notifications for enabled streams
    * Linked to #666 (probably only a partial fix so far)

--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -820,6 +820,10 @@ class TestModel:
                                            types_when_notify_called):
         message_fixture.update(vary_each_msg)
         model.user_id = user_id
+        if 'stream_id' in message_fixture:
+            model.stream_dict.update(
+                {message_fixture['stream_id']: {'desktop_notifications': True}}
+            )
         notify = mocker.patch('zulipterminal.model.notify')
         model.notify_user(message_fixture)
         if message_fixture['type'] in types_when_notify_called:
