bad1694fc2890b4a7bc5d3921c1d6ad195413199
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2020-07-23 11:36:47 -0700

    version: Update server version 3.0 feature level & status.
    Feature level bumped to match released server version at:
    https://zulip.com/api/changelog#changes-in-zulip-30

--- zulipterminal/version.py
+++ zulipterminal/version.py
@@ -2,7 +2,7 @@ ZT_VERSION = '0.5.1+git'
 
 SUPPORTED_SERVER_VERSIONS = [
     ('2.1', None),
-    ('3.0', 1),  # NOTE: 3.0 is not yet released.
+    ('3.0', 25),
 ]
 
 MINIMUM_SUPPORTED_SERVER_VERSION = SUPPORTED_SERVER_VERSIONS[0]
