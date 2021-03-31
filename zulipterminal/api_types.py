1142cb9186bae791d6df5ad06a012e23060f8cb9
Ezio-Sarthak <garg.sarthak13@gmail.com>
2021-03-31 11:10:02 +0530

    api_types: Support all Zulip 2.1+ topic links responses.
    `topic_links` param is new in Zulip 3.0 (feature level 1) release.
    Previously it was called as `subject_links`.
    
    Successful `topic_links` response earlier vs now:
     * earlier: ['www.link1.com']
     * now:     [{'url': 'www.link1.com', 'text': 'My link']}

--- zulipterminal/api_types.py
+++ zulipterminal/api_types.py
@@ -30,7 +30,12 @@ class Message(TypedDict, total=False):
     timestamp: int
     client: str
     subject: str  # Only for stream msgs.
-    topic_links: List[str]
+    # NOTE: new in Zulip 3.0 / ZFL 1
+    # NOTE: API response format of `topic_links` new in Zulip 4.0 / ZFL 46
+    topic_links: List[Any]
+    # NOTE: `subject_links` new in Zulip 2.1;
+    # Deprecated from Zulip 3.0 / ZFL 1
+    subject_links: List[str]
     is_me_message: bool
     reactions: List[Dict[str, Any]]
     submessages: List[Dict[str, Any]]
