a81c2471fc22971a637ab856643cde57ce8c9dbf
Ezio-Sarthak <garg.sarthak13@gmail.com>
2021-04-05 12:21:37 +0530

    refactor: views/ui_mappings: Extract status markers in a constant dict.
    This commit centralizes the use of state icons corresponding to
    a user status in `ui_mappings.py` as `STATE_ICON`.

--- zulipterminal/config/ui_mappings.py
+++ zulipterminal/config/ui_mappings.py
@@ -1,10 +1,25 @@
 from typing import Dict
 
 from zulipterminal.api_types import EditPropagateMode
+from zulipterminal.config.symbols import (
+    STATUS_ACTIVE,
+    STATUS_IDLE,
+    STATUS_INACTIVE,
+    STATUS_OFFLINE,
+)
 
 
 EDIT_MODE_CAPTIONS: Dict[EditPropagateMode, str] = {
     "change_one": "Change only this message topic",
     "change_later": "Also change later messages to this topic",
     "change_all": "Also change previous and following messages to this topic",
+}
+
+
+# Mapping that binds user activity status to corresponding markers.
+STATE_ICON = {
+    "active": STATUS_ACTIVE,
+    "idle": STATUS_IDLE,
+    "offline": STATUS_OFFLINE,
+    "inactive": STATUS_INACTIVE,
 }
