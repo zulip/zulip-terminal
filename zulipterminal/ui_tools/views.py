8a2437907fb9cce19328404455bf0ce6b10bf555
Ezio-Sarthak <grgsrthk.dev20@gmail.com>
2021-05-08 21:37:15 +0530

    views: Structure UI in MarkdownHelpView class.
    This commit adds the class responsible to show markdown help. It
    fetches markdown data from the newly created `markdown_examples.py`.
    
    Tests added for MarkdownHelpView.

--- zulipterminal/ui_tools/views.py
+++ zulipterminal/ui_tools/views.py
@@ -15,6 +15,7 @@ from zulipterminal.config.keys import (
     keys_for_command,
     primary_key_for_command,
 )
+from zulipterminal.config.markdown_examples import MARKDOWN_ELEMENTS
 from zulipterminal.config.symbols import (
     CHECK_MARK,
     COLUMN_TITLE_BAR_LINE,
@@ -1229,6 +1230,41 @@ class HelpView(PopUpView):
         widgets = self.make_table_with_categories(help_menu_content, column_widths)
 
         super().__init__(controller, widgets, "HELP", popup_width, title)
+
+
+class MarkdownHelpView(PopUpView):
+    def __init__(self, controller: Any, title: str) -> None:
+        raw_menu_content = []  # to calculate table dimensions
+        rendered_menu_content = []  # to display rendered content in table
+        user_name = controller.model.user_full_name
+
+        for element in MARKDOWN_ELEMENTS:
+            raw_content = element["raw_text"]
+            html_element = element["html_element"].format(**dict(user=user_name))
+
+            rendered_content, *_ = MessageBox.transform_content(
+                html_element, controller.model.server_url
+            )
+
+            raw_menu_content.append((raw_content, raw_content))
+            rendered_menu_content.append((raw_content, rendered_content))
+
+        popup_width, column_widths = self.calculate_table_widths(
+            [("", raw_menu_content)], len(title)
+        )
+
+        header_widgets = [
+            urwid.Text([("popup_category", "You type")], align="center"),
+            urwid.Text([("popup_category", "You get")], align="center"),
+        ]
+        header_columns = urwid.Columns(header_widgets)
+        header = urwid.Pile([header_columns, urwid.Divider(COLUMN_TITLE_BAR_LINE)])
+
+        body = self.make_table_with_categories(
+            [("", rendered_menu_content)], column_widths
+        )
+
+        super().__init__(controller, body, "MARKDOWN_HELP", popup_width, title, header)
 
 
 class PopUpConfirmationView(urwid.Overlay):
