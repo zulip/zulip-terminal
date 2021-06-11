1873e0ef196bb6759617de6f7c466655757f74a2
Rohitth <rohitthc123456789@gmail.com>
2021-06-11 19:59:05 +0530

    zt_dark: Create new zt_dark theme structure.
    New zt_dark theme structure was created using a script and
    some manual editing. `zt_dark.py` can be found in the new themes folder.

---
+++ zulipterminal/themes/zt_dark.py
@@ -0,0 +1,64 @@
+"""
+ZT DARK
+-------
+This theme uses the default color scheme.
+For further details on themefiles look at the theme contribution guide.
+"""
+from zulipterminal.config.color import DefaultBoldColor as Color
+
+
+# fmt: off
+STYLES = {
+    # style_name      :  foreground                 background
+    None              : (Color.WHITE,               Color.BLACK),
+    'selected'        : (Color.WHITE,               Color.DARK_BLUE),
+    'msg_selected'    : (Color.WHITE,               Color.DARK_BLUE),
+    'header'          : (Color.DARK_CYAN,           Color.DARK_BLUE),
+    'general_narrow'  : (Color.WHITE,               Color.DARK_BLUE),
+    'general_bar'     : (Color.WHITE,               Color.BLACK),
+    'name'            : (Color.YELLOW__BOLD,        Color.BLACK),
+    'unread'          : (Color.DARK_BLUE,           Color.BLACK),
+    'user_active'     : (Color.LIGHT_GREEN,         Color.BLACK),
+    'user_idle'       : (Color.YELLOW,              Color.BLACK),
+    'user_offline'    : (Color.WHITE,               Color.BLACK),
+    'user_inactive'   : (Color.WHITE,               Color.BLACK),
+    'title'           : (Color.WHITE__BOLD,         Color.BLACK),
+    'column_title'    : (Color.WHITE__BOLD,         Color.BLACK),
+    'time'            : (Color.LIGHT_BLUE,          Color.BLACK),
+    'bar'             : (Color.WHITE,               Color.DARK_GRAY),
+    'popup_contrast'  : (Color.WHITE,               Color.DARK_GRAY),
+    'msg_emoji'       : (Color.LIGHT_MAGENTA,       Color.BLACK),
+    'reaction'        : (Color.LIGHT_MAGENTA__BOLD, Color.BLACK),
+    'reaction_mine'   : (Color.BLACK,               Color.LIGHT_MAGENTA),
+    'msg_mention'     : (Color.LIGHT_RED__BOLD,     Color.BLACK),
+    'msg_link'        : (Color.LIGHT_BLUE,          Color.BLACK),
+    'msg_link_index'  : (Color.LIGHT_BLUE__BOLD,    Color.BLACK),
+    'msg_quote'       : (Color.BROWN,               Color.BLACK),
+    'msg_code'        : (Color.BLACK,               Color.WHITE),
+    'msg_bold'        : (Color.WHITE__BOLD,         Color.BLACK),
+    'msg_time'        : (Color.BLACK,               Color.WHITE),
+    'footer'          : (Color.BLACK,               Color.LIGHT_GRAY),
+    'footer_contrast' : (Color.WHITE,               Color.BLACK),
+    'starred'         : (Color.LIGHT_RED__BOLD,     Color.BLACK),
+    'popup_category'  : (Color.LIGHT_BLUE__BOLD,    Color.BLACK),
+    'unread_count'    : (Color.YELLOW,              Color.BLACK),
+    'starred_count'   : (Color.LIGHT_GRAY,          Color.BLACK),
+    'table_head'      : (Color.WHITE__BOLD,         Color.BLACK),
+    'filter_results'  : (Color.WHITE,               Color.DARK_GREEN),
+    'edit_topic'      : (Color.WHITE,               Color.DARK_GRAY),
+    'edit_tag'        : (Color.WHITE,               Color.DARK_GRAY),
+    'edit_author'     : (Color.YELLOW,              Color.BLACK),
+    'edit_time'       : (Color.LIGHT_BLUE,          Color.BLACK),
+    'current_user'    : (Color.WHITE,               Color.BLACK),
+    'muted'           : (Color.LIGHT_BLUE,          Color.BLACK),
+    'popup_border'    : (Color.WHITE,               Color.BLACK),
+    'area:help'       : (Color.WHITE,               Color.DARK_GREEN),
+    'area:msg'        : (Color.WHITE,               Color.BROWN),
+    'area:stream'     : (Color.WHITE,               Color.DARK_CYAN),
+    'area:error'      : (Color.WHITE,               Color.DARK_RED),
+    'search_error'    : (Color.LIGHT_RED,           Color.BLACK),
+    'task:success'    : (Color.WHITE,               Color.DARK_GREEN),
+    'task:error'      : (Color.WHITE,               Color.DARK_RED),
+    'task:warning'    : (Color.WHITE,               Color.BROWN),
+}
+# fmt: on
