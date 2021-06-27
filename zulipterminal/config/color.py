d31e7ca728f18da77f8b832ec462752bef5b1692
Rohitth <rohitthc123456789@gmail.com>
2021-06-27 20:14:28 +0530

    color: Add default color-scheme with bold, based on existing colors.
    A DefaultColor class is added to color.py which acts as the default
    color-scheme for ZT. The BOLD property is added to these colors
    using the helper function color_properties.
    
    The format:
    * The colors have been defined using Enum's as a single string
    containing 16, 256 and 24 bit codes.
    * Each style is defined as a tuple of 2 elements, the foreground
    and the background.
    
    The 24 bit colors are not defined for the default colors hence
    default is used. 2 missing colors in DEF, DARK_MAGENTA and
    LIGHT_CYAN, which were used in zt_blue were added into Color.

--- zulipterminal/config/color.py
+++ zulipterminal/config/color.py
@@ -8,6 +8,33 @@ from enum import Enum
 from typing import Any
 
 
+# fmt: off
+# NOTE: The 24bit color codes use 256 color which can be
+# enhanced to be truly 24bit.
+# NOTE: The 256code format can be moved to h0-255 to
+# make use of the complete range instead of only 216 colors.
+class DefaultColor(Enum):
+    # color       =  16code          256code    24code
+    DEFAULT       = 'default         default    default'
+    BLACK         = 'black           g19        g19'
+    DARK_RED      = 'dark_red        #a00       #a00'
+    DARK_GREEN    = 'dark_green      #080       #080'
+    BROWN         = 'brown           #880       #880'
+    DARK_BLUE     = 'dark_blue       #24a       #24a'
+    DARK_MAGENTA  = 'dark_magenta    h90        h90'      # #870087
+    DARK_CYAN     = 'dark_cyan       #088       #088'
+    DARK_GRAY     = 'dark_gray       #666       #666'
+    LIGHT_RED     = 'light_red       #f00       #f00'
+    LIGHT_GREEN   = 'light_green     #0f0       #0f0'
+    YELLOW        = 'yellow          #ff0       #ff0'
+    LIGHT_BLUE    = 'light_blue      #28d       #28d'
+    LIGHT_MAGENTA = 'light_magenta   #c8f       #c8f'
+    LIGHT_CYAN    = 'light_cyan      h152       h152'     # #afd7d7
+    LIGHT_GRAY    = 'light_gray      #ccc       #ccc'
+    WHITE         = 'white           #fff       #fff'
+# fmt: on
+
+
 def color_properties(colors: Any, *prop: str) -> Any:
     """
     Adds properties(Bold, Italics, etc...) to Enum Colors in theme files.
@@ -31,3 +58,6 @@ def color_properties(colors: Any, *prop: str) -> Any:
         },
     )
     return updated_colors
+
+
+DefaultBoldColor = color_properties(DefaultColor, "BOLD")
