78f42e617ec905845432744cb9e579274f0899d5
prah23 <harry.prashantb@gmail.com>
2021-07-06 20:20:54 +0530

    refactor: boxes: Use helper method to set/reset recipient `user_id`s.
    This commit adds a helper method to synchronously set both types of
    recipient user_ids stored in WriteBox instances, namely,
    `recipient_user_id`s and `typing_recipient_user_ids`.

--- zulipterminal/ui_tools/boxes.py
+++ zulipterminal/ui_tools/boxes.py
@@ -116,6 +116,20 @@ class WriteBox(urwid.Pile):
     def set_editor_mode(self) -> None:
         self.view.controller.enter_editor_mode_with(self)
 
+    def _set_regular_and_typing_recipient_user_ids(
+        self, user_id_list: Optional[List[int]] = None
+    ) -> None:
+        if user_id_list:
+            self.recipient_user_ids = user_id_list
+            self.typing_recipient_user_ids = [
+                user_id
+                for user_id in self.recipient_user_ids
+                if user_id != self.model.user_id
+            ]
+        else:
+            self.recipient_user_ids = list()
+            self.typing_recipient_user_ids = list()
+
     def send_stop_typing_status(self) -> None:
         # Send 'stop' updates only for PM narrows.
         if self.to_write_box and self.typing_recipient_user_ids:
@@ -139,7 +153,7 @@ class WriteBox(urwid.Pile):
         self.set_editor_mode()
 
         if recipient_user_ids and emails:
-            self.recipient_user_ids = recipient_user_ids
+            self._set_regular_and_typing_recipient_user_ids(recipient_user_ids)
             self.recipient_emails = emails
             recipient_info = ", ".join(
                 [
@@ -147,14 +161,8 @@ class WriteBox(urwid.Pile):
                     for email in emails
                 ]
             )
-            self.typing_recipient_user_ids = [
-                user_id
-                for user_id in self.recipient_user_ids
-                if user_id != self.model.user_id
-            ]
         else:
-            self.recipient_user_ids = []
-            self.typing_recipient_user_ids = []
+            self._set_regular_and_typing_recipient_user_ids()
             self.recipient_emails = []
             recipient_info = ""
 
@@ -739,14 +747,9 @@ class WriteBox(urwid.Pile):
                     # invalid ones.
                     self.update_recipient_emails(self.to_write_box)
                     users = self.model.user_dict
-                    self.recipient_user_ids = [
-                        users[email]["user_id"] for email in self.recipient_emails
-                    ]
-                    self.typing_recipient_user_ids = [
-                        user_id
-                        for user_id in self.recipient_user_ids
-                        if user_id is not self.model.user_id
-                    ]
+                    self._set_regular_and_typing_recipient_user_ids(
+                        [users[email]["user_id"] for email in self.recipient_emails]
+                    )
 
             if not self.msg_body_edit_enabled:
                 return key
