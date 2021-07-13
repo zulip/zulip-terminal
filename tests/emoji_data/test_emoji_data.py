ad8e48906602b5bf4460da41911c2042619d00ec
prah23 <harry.prashantb@gmail.com>
2021-07-14 03:55:07 +0530

    refactor: tests: emoji_data: Add type annotations.
    This commit adds parameter and return type annotations or hints to the
    `test_emoji_data.py` file, that contains tests for its counterpart
    `unicode_emoji.py` from  the `zulipterminal` module, to make mypy
    checks consistent and improve code readability.

--- tests/emoji_data/test_emoji_data.py
+++ tests/emoji_data/test_emoji_data.py
@@ -1,11 +1,14 @@
 from collections import OrderedDict
+from typing import Dict
 
 from zulipterminal.unicode_emojis import EMOJI_DATA
 
 
-def test_generated_emoji_list_sorted():
+def test_generated_emoji_list_sorted() -> None:
     assert EMOJI_DATA == OrderedDict(sorted(EMOJI_DATA.items()))
 
 
-def test_unicode_emojis_fixture_sorted(unicode_emojis):
+def test_unicode_emojis_fixture_sorted(
+    unicode_emojis: "OrderedDict[str, Dict[str, str]]",
+) -> None:
     assert unicode_emojis == OrderedDict(sorted(unicode_emojis.items()))
