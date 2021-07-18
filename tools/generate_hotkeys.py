1a125008efe4c4af50defc5cf02607e356ad73a4
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2021-07-17 18:03:54 -0700

    refactor: Reformat tools/ with black, except quotes.
    NOTE: tools/lister.py was imported from zulip/zulip as a helper for
    tools/run-mypy and has not been edited directly significantly
    subsequently.
    
    As such, no specific allowances are made in applying black to lister.py,
    since black preserves functionality and in future we expect to move to
    the lister functionality present in zulint.

--- tools/generate_hotkeys.py
+++ tools/generate_hotkeys.py
@@ -6,7 +6,7 @@ from zulipterminal.config.keys import HELP_CATEGORIES, KEY_BINDINGS
 
 categories = {
     category: {
-        item['help_text']: item['keys'] 
+        item['help_text']: item['keys']
         for item in KEY_BINDINGS.values()
         if item['key_category'] == category
     }
@@ -15,21 +15,24 @@ categories = {
 OUTPUT_FILE = Path(__file__).resolve().parent.parent / 'docs' / 'hotkeys.md'
 SCRIPT_NAME = PurePath(__file__).name
 
-with open(OUTPUT_FILE,"w") as mdFile:
-    mdFile.write(f"<!--- Generated automatically by tools/{SCRIPT_NAME} -->\n"
-                  "<!--- Do not modify -->\n\n# Hot Keys\n")
+with open(OUTPUT_FILE, "w") as mdFile:
+    mdFile.write(
+        f"<!--- Generated automatically by tools/{SCRIPT_NAME} -->\n"
+        "<!--- Do not modify -->\n\n# Hot Keys\n"
+    )
     for action in categories.keys():
-        mdFile.write(f"## {HELP_CATEGORIES[action]}\n"
-                      "|Command|Key Combination|\n"
-                      "| :--- | :---: |\n")
+        mdFile.write(
+            f"## {HELP_CATEGORIES[action]}\n"
+            "|Command|Key Combination|\n"
+            "| :--- | :---: |\n"
+        )
         for help_text, key_combinations_list in categories[action].items():
-            various_key_combinations = " / ".join([
-                " + ".join([
-                    f"<kbd>{key}</kbd>"
-                    for key in key_combination.split()
-                ])
-                for key_combination in key_combinations_list
-            ])
+            various_key_combinations = " / ".join(
+                [
+                    " + ".join([f"<kbd>{key}</kbd>" for key in key_combination.split()])
+                    for key_combination in key_combinations_list
+                ]
+            )
             mdFile.write(f"|{help_text}|{various_key_combinations}|\n")
         mdFile.write("\n")
 
