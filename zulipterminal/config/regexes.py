363ed9eec3675743a707f24f96fc9a8464dff581
Ezio-Sarthak <grgsrthk.dev20@gmail.com>
2021-07-26 09:22:56 +0530

    refactor: boxes/regexes: Centralize and use PM recipient regexes.

--- zulipterminal/config/regexes.py
+++ zulipterminal/config/regexes.py
@@ -7,3 +7,9 @@ REGEX_QUOTED_FENCE_LENGTH = r"^ {0,3}(`{3,})"
 REGEX_COLOR_3_DIGIT = r"^#[0-9A-Fa-f]{3}$"
 # Example: #ffffff
 REGEX_COLOR_6_DIGIT = r"^#[0-9A-Fa-f]{6}$"
+
+
+# Example: example-user@zulip.com
+REGEX_RECIPIENT_EMAIL = r"[\w\.-]+@[\w\.-]+"
+# Example: Test User <example-user@zulip.com>
+REGEX_CLEANED_RECIPIENT = r"^(.*?)(?:\s*?<?({})>?(.*))?$".format(REGEX_RECIPIENT_EMAIL)
