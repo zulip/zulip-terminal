d2c00002c950a818e40ebcbead9fcc7484206915
Ezio-Sarthak <grgsrthk.dev20@gmail.com>
2021-07-27 11:48:07 +0530

    refactor: urwid_types: Add type annotation for `urwid.Text` markups.
    This commit adds a general annotation for urwid.Text markup content.
    Since the urwid markup is used almost throughout the UI part of the
    codebase, it would be nice to annotate it as we move ahead with
    slightly complex UI features and widgets.

--- zulipterminal/urwid_types.py
+++ zulipterminal/urwid_types.py
@@ -1,7 +1,20 @@
-from typing import Tuple, Union
+from typing import Any, Optional, Sequence, Tuple, Union
 
 
 urwid_Fixed = Tuple[()]
 urwid_Flow = Tuple[int]
 urwid_Box = Tuple[int, int]
 urwid_Size = Union[urwid_Fixed, urwid_Flow, urwid_Box]
+
+TextMarkupType = Union[
+    # e.g., "hello"
+    str,
+    # e.g., ["This", "is", "a", "text"]
+    Sequence[str],
+    # e.g., ("red", "Styled text")
+    Tuple[Optional[str], Any],
+    # e.g., [(None, "Not styled"), ("red", "Styled")]
+    Sequence[Tuple[Optional[str], Any]],
+]
+
+urwidTextMarkup = Union[TextMarkupType, Sequence[TextMarkupType]]
