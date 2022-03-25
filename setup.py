4bb4fb8c034ec6132c1dfb2c34fb9f01d022fae9
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2022-03-25 11:50:45 -0700

    requirements: Upgrade lxml from >=4.6.2 to >=4.7.1.

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ setup(
         "zulip>=0.7.0",
         "urwid_readline>=0.13",
         "beautifulsoup4>=4.9.0",
-        "lxml>=4.6.2",
+        "lxml>=4.7.1",
         "pygments>=2.11.2",
         "typing_extensions>=3.7",
         "python-dateutil>=2.8.2",
