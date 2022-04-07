070eaa08dc9004785fbccac34ea57b55aee29ceb
Progyan <progyan.das@iitgn.ac.in>
2022-04-07 19:32:58 +0530

    feature: Add presence markers to PM compose.
    Fixes part of #1155.
    
    PMs didn't have a way to know whether the user was active without presence markers from the right column view, which is a hassle. This allows them to see it directly.
    
    Need to add colour to the markers.
    
    In the future, maybe add some form of last seen text that's shorter than just the timestamp ("last seen 3 hours ago") instead of ("last seen on Fri 3 Aug 03-02-2002").

--- zulipterminal/ui_tools/boxes.py
+++ zulipterminal/ui_tools/boxes.py
@@ -201,7 +201,7 @@ class WriteBox(urwid.Pile):
             ]
             recipient_info = ", ".join(
                 [
-                    f"{self.model.user_dict[email]['full_name']} <{email}>"
+                    f"""{STATE_ICON[self.model.user_dict.get(email, None).get("status","inactive") if self.model.user_dict.get(email,None) else "inactive"]} {self.model.user_dict[email]['full_name']} <{email}>"""
                     for email in self.recipient_emails
                 ]
             )
@@ -209,8 +209,6 @@ class WriteBox(urwid.Pile):
             self._set_regular_and_typing_recipient_user_ids(None)
             self.recipient_emails = []
             recipient_info = ""
-
-        self.send_next_typing_update = datetime.now()
         self.to_write_box = ReadlineEdit("To: ", edit_text=recipient_info)
         self.to_write_box.enable_autocomplete(
             func=self._to_box_autocomplete,
