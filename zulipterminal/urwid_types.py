7eb3ca4e2a17a11a8cd8a7267b38c0de9192679c
Rohitth <rohitthc123456789@gmail.com>
2022-06-26 16:53:01 +0530

    urwid_types: Add type `urwid_MarkupTuple`.
    Added new urwid type `urwid_MarkupTuple` which is a
    `Tuple[Optional[str], str]`. This denotes the most basic styled
    markup that urwid accepts.

--- zulipterminal/urwid_types.py
+++ zulipterminal/urwid_types.py
@@ -2,10 +2,12 @@
 Preliminary urwid types to improve type analysis
 """
 
-from typing import Tuple, Union
+from typing import Optional, Tuple, Union
 
 
 urwid_Fixed = Tuple[()]
 urwid_Flow = Tuple[int]
 urwid_Box = Tuple[int, int]
 urwid_Size = Union[urwid_Fixed, urwid_Flow, urwid_Box]
+
+urwid_MarkupTuple = Tuple[Optional[str], str]
