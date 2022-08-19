63058a08b3489f65ee42170a2b7f5aaf55bdc7c3
srdeotarse <srdeotarse@gmail.com>
2022-08-19 11:25:24 +0530

    refactor: generate_hotkeys: Change categories type to defaultdict(list).
    Loop through KEY_BINDINGS only once and use order of HELP_CATEGORIES
    list for hotkeys.md

--- tools/generate_hotkeys.py
+++ tools/generate_hotkeys.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
+from collections import defaultdict
 from pathlib import Path, PurePath
 
 from zulipterminal.config.keys import HELP_CATEGORIES, KEY_BINDINGS
 
 
-categories = {
-    category: {
-        item["help_text"]: item["keys"]
-        for item in KEY_BINDINGS.values()
-        if item["key_category"] == category
-    }
-    for category in HELP_CATEGORIES.keys()
-}
+categories = defaultdict(list)
+for item in KEY_BINDINGS.values():
+    categories[item["key_category"]].append((item["help_text"], item["keys"]))
+
 OUTPUT_FILE = Path(__file__).resolve().parent.parent / "docs" / "hotkeys.md"
 SCRIPT_NAME = PurePath(__file__).name
 
@@ -20,13 +17,13 @@ with open(OUTPUT_FILE, "w") as mdFile:
         f"<!--- Generated automatically by tools/{SCRIPT_NAME} -->\n"
         "<!--- Do not modify -->\n\n# Hot Keys\n"
     )
-    for action in categories.keys():
+    for action in HELP_CATEGORIES.keys():
         mdFile.write(
             f"## {HELP_CATEGORIES[action]}\n"
             "|Command|Key Combination|\n"
             "| :--- | :---: |\n"
         )
-        for help_text, key_combinations_list in categories[action].items():
+        for help_text, key_combinations_list in categories[action]:
             various_key_combinations = " / ".join(
                 [
                     " + ".join([f"<kbd>{key}</kbd>" for key in key_combination.split()])
