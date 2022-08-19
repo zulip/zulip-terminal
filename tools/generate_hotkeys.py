311085ea6a55215dfabae91d9177d1822c329c2e
srdeotarse <srdeotarse@gmail.com>
2022-08-19 10:54:44 +0530

    refactor: generate_hotkeys: Split string construction & file writing.
    Store the hotkeys.md file string in variable hotkeys_file_string
    extracted from config/keys.py and write the variable at the end.
    This is a refactor from earlier approach of writing directly to
    the hotkeys.md file multiple times.

--- tools/generate_hotkeys.py
+++ tools/generate_hotkeys.py
@@ -12,25 +12,26 @@ for item in KEY_BINDINGS.values():
 OUTPUT_FILE = Path(__file__).resolve().parent.parent / "docs" / "hotkeys.md"
 SCRIPT_NAME = PurePath(__file__).name
 
-with open(OUTPUT_FILE, "w") as mdFile:
-    mdFile.write(
-        f"<!--- Generated automatically by tools/{SCRIPT_NAME} -->\n"
-        "<!--- Do not modify -->\n\n# Hot Keys\n"
+hotkeys_file_string = (
+    f"<!--- Generated automatically by tools/{SCRIPT_NAME} -->\n"
+    "<!--- Do not modify -->\n\n# Hot Keys\n"
+)
+for action in HELP_CATEGORIES.keys():
+    hotkeys_file_string += (
+        f"## {HELP_CATEGORIES[action]}\n"
+        "|Command|Key Combination|\n"
+        "| :--- | :---: |\n"
     )
-    for action in HELP_CATEGORIES.keys():
-        mdFile.write(
-            f"## {HELP_CATEGORIES[action]}\n"
-            "|Command|Key Combination|\n"
-            "| :--- | :---: |\n"
+    for help_text, key_combinations_list in categories[action]:
+        various_key_combinations = " / ".join(
+            [
+                " + ".join([f"<kbd>{key}</kbd>" for key in key_combination.split()])
+                for key_combination in key_combinations_list
+            ]
         )
-        for help_text, key_combinations_list in categories[action]:
-            various_key_combinations = " / ".join(
-                [
-                    " + ".join([f"<kbd>{key}</kbd>" for key in key_combination.split()])
-                    for key_combination in key_combinations_list
-                ]
-            )
-            mdFile.write(f"|{help_text}|{various_key_combinations}|\n")
-        mdFile.write("\n")
+        hotkeys_file_string += f"|{help_text}|{various_key_combinations}|\n"
+    hotkeys_file_string += "\n"
+with open(OUTPUT_FILE, "w") as mdFile:
+    mdFile.write(hotkeys_file_string)
 
 print(f"Hot Keys list saved in {OUTPUT_FILE}")
