6cfeae46024e94bb03480a296269ba0f3aaf7827
mounilKshah <mkshah_b18@ce.vjti.ac.in>
2022-08-28 07:21:09 +0530

    model/test_model: Add function to handle realm_user event update.
    This commit contains Model._handle_realm_user_event()
    which updates the meta data of the respective user in
    Model.initia_data["realm_users"]. It also contains test
    for this function in test_model.

--- zulipterminal/model.py
+++ zulipterminal/model.py
@@ -154,6 +154,7 @@ class Model:
                 ("update_display_settings", self._handle_update_display_settings_event),
                 ("user_settings", self._handle_user_settings_event),
                 ("realm_emoji", self._handle_update_emoji_event),
+                ("realm_user", self._handle_realm_user_event),
             ]
         )
 
@@ -1829,6 +1830,21 @@ class Model:
                 msg_w_list = create_msg_box_list(self, [msg_id], last_message=last_msg)
                 view.message_view.log[msg_pos] = msg_w_list[0]
         self.controller.update_screen()
+
+    def _handle_realm_user_event(self, event: Event) -> None:
+        """
+        Handle change to user(s) metadata (Eg: full_name, timezone, etc.)
+        """
+        assert event["type"] == "realm_user"
+        if event["op"] == "update":
+            # realm_users has 'email' attribute and not 'new_email'
+            if "new_email" in event["person"]:
+                event["person"]["email"] = event["person"].pop("new_email")
+            updated_details = event["person"]
+            for realm_user in self.initial_data["realm_users"]:
+                if realm_user["user_id"] == updated_details["user_id"]:
+                    realm_user.update(updated_details)
+                    break
 
     def _register_desired_events(self, *, fetch_data: bool = False) -> str:
         fetch_types = None if not fetch_data else self.initial_data_to_fetch
