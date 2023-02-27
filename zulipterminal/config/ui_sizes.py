1fcdf3b254ba57d19313b94bce6ab0d11ca60249
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2023-02-26 16:54:59 -0800

    refactor: ui_sizes/core: Migrate popup scaling constants into ui_sizes.

--- zulipterminal/config/ui_sizes.py
+++ zulipterminal/config/ui_sizes.py
@@ -5,3 +5,10 @@ Fixed sizes of UI elements
 TAB_WIDTH = 3
 LEFT_WIDTH = 29
 RIGHT_WIDTH = 23
+
+# These affect popup width-scaling, dependent upon window width
+# At and below this width, popup is window width:
+MIN_SUPPORTED_POPUP_WIDTH = 80
+# Until this width, popup scales linearly upwards with width
+MAX_LINEAR_SCALING_WIDTH = 100
+# Above that width, popup increases as 3/4 of window width
