d50bd4c58fd00319d8762f46a7d377245f0a4e33
Sushmey <sushmeywork@gmail.com>
2023-03-20 10:03:06 +0530

    Test updated: Add conditional check for starred message in same test

--- tests/ui_tools/test_messages.py
+++ tests/ui_tools/test_messages.py
@@ -1074,8 +1074,12 @@ class TestMessageBox:
         varied_message = dict(message_fixture, **to_vary_in_each_message)
         msg_box = MessageBox(varied_message, self.model, varied_message)
         view_components = msg_box.main_view()
-        assert len(view_components) == 1
-        assert isinstance(view_components[0], Padding)
+        if "starred" in varied_message["flags"]:
+            assert len(view_components) == 2
+            assert isinstance(view_components[0], Columns)
+        else:
+            assert len(view_components) == 1
+            assert isinstance(view_components[0], Padding)
 
     def test_main_view_generates_EDITED_label(
         self, mocker, messages_successful_response
