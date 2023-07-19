5927ed867768362b705925c78904a7bea753cfb7
Subhasish-Behera <greendoor3000@gmail.com>
2023-07-20 00:43:36 +0530

    model: Add function to upload file.
    get_file_upld_uri takes a file location as an argument and returns the URI.

--- zulipterminal/model.py
+++ zulipterminal/model.py
@@ -560,6 +560,11 @@ class Model:
             notify_if_message_sent_outside_narrow(composition, self.controller)
         return message_was_sent
 
+    def get_file_upload_uri(self, file_location: str) -> str:
+        with open(file_location, "rb") as fp:
+            result = self.client.upload_file(fp)
+        return result["uri"]
+
     def update_private_message(self, msg_id: int, content: str) -> bool:
         request: PrivateMessageUpdateRequest = {
             "message_id": msg_id,
