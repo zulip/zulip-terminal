514db27fdb7260061f59629dc114962d9be809c0
Niloth-p <20315308+Niloth-p@users.noreply.github.com>
2024-05-12 20:55:56 +0530

    lint/regex: keys: Add linting for usage of external commands.
    Identify external commands using the added suffix.
    
    Search for both the suffix variable and the actual suffix.

--- zulipterminal/config/regexes.py
+++ zulipterminal/config/regexes.py
@@ -7,6 +7,9 @@ Regular expression constants
 
 
 # (*) Stream and topic regexes
+from zulipterminal.config.keys import READLINE_SUFFIX
+
+
 REGEX_STREAM_NAME = r"([^*>]+)"
 REGEX_TOPIC_NAME = r"([^*]*)"
 
@@ -46,3 +49,7 @@ REGEX_COLOR_VALID_FORMATS = (
 
 # Example: 6-test-stream
 REGEX_INTERNAL_LINK_STREAM_ID = r"^[0-9]+-"
+
+
+# Example: UNDO_LAST_ACTION_READLINE
+REGEX_READLINE_COMMANDS = rf"([A-Z_]+{READLINE_SUFFIX})"
