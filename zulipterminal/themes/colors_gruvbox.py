dd3d466bedd9f91391b81b3b0b9a446ca4310279
Gopinath-Mahendiran <gopinathmahendiran@gmail.com>
2025-04-01 09:52:39 +0530

    lint : fix the ruff linting issue.

--- zulipterminal/themes/colors_gruvbox.py
+++ zulipterminal/themes/colors_gruvbox.py
@@ -8,6 +8,7 @@ This file uses the official gruvbox colors where possible.
 For color reference see:
     https://github.com/morhetz/gruvbox/blob/master/colors/gruvbox.vim
 """
+
 from enum import Enum
 
 from zulipterminal.config.color import color_properties
@@ -25,12 +26,27 @@ class GruvBoxColor(Enum):
     # Only or primarily dark mode - grayscales
     # - generally background
     DARK0_HARD       = 'black           h234      #1d2021'
+    DARK0            = 'black           h235      #282828'
+    DARK0_SOFT       = 'black           h236      #32302f'
+    DARK1            = 'black           h237      #3c3836'
+    DARK2            = 'black           h239      #504945'
+    DARK3            = 'black           h241      #665c54'
+    DARK4            = 'black           h243      #7c6f64'
     # - generally foreground
     LIGHT2           = 'white           h250      #d5c4a1'
     # - grays
     GRAY_244         = 'dark_gray       h244      #928374'
+    GRAY_245         = 'dark_gray       h245      #928374'
     LIGHT4           = 'light_gray      h248      #bdae93'
 
+    # Only or primarily light version - grayscales
+    # - generally background
+    LIGHT0_HARD      = 'white           h230      #f9f5d7'
+    LIGHT0           = 'white           h229      #fbf1c7'
+    LIGHT0_SOFT      = 'white           h228      #f2e5bc'
+    LIGHT1           = 'white           h223      #ebdbb2'
+    LIGHT4_256       = 'light_gray      h246      #a89984'
+   
     # Dark mode only - colors
     BRIGHT_BLUE      = 'light_blue      h109      #83a598'
     BRIGHT_GREEN     = 'light_green     h142      #b8bb26'
@@ -48,15 +64,17 @@ class GruvBoxColor(Enum):
     FADED_RED        = 'dark_red        h88       #9d0006'
     FADED_YELLOW     = 'brown           h136      #b57614'
 
-    # Only or primarily light version - grayscales
-    # - generally background
-    LIGHT0_HARD      = 'white           h230      #f9f5d7'
-    # - generally foreground
-    DARK2            = 'black           h239      #504945'
-    # - grays
-    GRAY_245         = 'dark_gray       h245      #928374'
-    DARK4            = 'black           h243      #7c6f64'
-
+    # Additional colors
+    BRIGHT_PURPLE    = 'light_magenta   h175      #d3869b'
+    BRIGHT_AQUA      = 'light_cyan      h108      #8ec07c'
+    BRIGHT_ORANGE    = 'light_red       h208      #fe8019'
+    NEUTRAL_RED      = 'dark_red        h124      #cc241d'
+    NEUTRAL_GREEN    = 'dark_green      h106      #98971a'
+    NEUTRAL_AQUA     = 'dark_cyan       h72       #689d6a'
+    NEUTRAL_ORANGE   = 'dark_red        h166      #d65d0e'
+    FADED_PURPLE     = 'dark_magenta    h96       #8f3f71'
+    FADED_AQUA       = 'dark_cyan       h66       #427b58'
+    FADED_ORANGE     = 'dark_red        h130      #af3a03'
 
 # fmt: on
 
