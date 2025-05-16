4f56fbc11991450ceddc4a871200a2a733a38cff
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2025-05-16 11:22:28 -0700

    requirements: Limit zulip upgrades from >=0.8.2 until Python 3.9.
    Zulip 0.9.0 library release requires Python 3.9.

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ setup(
     tests_require=testing_deps,
     install_requires=[
         "urwid~=2.1.2",
-        "zulip>=0.8.2",
+        "zulip>=0.8.2,<0.9.0",  # Next release, 0.9.0, requires Python 3.9
         "urwid_readline>=0.13",
         "beautifulsoup4>=4.11.1",
         "lxml~=4.9.2",
