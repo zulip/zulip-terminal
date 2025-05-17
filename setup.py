49d45d8fb8731d59a9a28070da0f9092c09463da
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2025-05-16 17:16:34 -0700

    requirements[dev]: Upgrade typos from ~=1.16.11 to >=1.32.0.
    This comfortably maintains installation under lower versions of Python,
    so move to a simple version constraint.
    
    Slightly adjust the title of a FAQ entry, which otherwise generates an
    error with the upgraded typos tool.
    
    Add constraint on codespell, where upgraded versions would fail on
    Python 3.7.

--- setup.py
+++ setup.py
@@ -32,8 +32,8 @@ linting_deps = [
     "isort~=5.11.0,<5.12.0",  # 5.12.0 drops support for Python 3.7, 6.0.0 drops 3.8
     "black==23.3.0",  # Later versions drop support for Python 3.7
     "ruff==0.0.267",
-    "codespell[toml]~=2.2.5",
-    "typos~=1.16.11",
+    "codespell[toml]==2.2.5,<2.2.6",  # 2.2.6 drops support for Python 3.7
+    "typos>=1.32.0",
 ]
 
 gitlint_deps = [
