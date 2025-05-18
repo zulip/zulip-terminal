44a5d6df046a8f672e068e850e5a57e19d1e6295
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2025-05-18 15:59:38 -0700

    requirements: Upgrade lxml.

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ setup(
         "zulip>=0.8.2",
         "urwid_readline>=0.13",
         "beautifulsoup4>=4.11.1",
-        "lxml~=4.9.2",
+        "lxml~=5.3.1",
         "pygments>=2.14.0,<2.18.0",
         "typing_extensions~=4.5.0",
         "python-dateutil>=2.8.2",
