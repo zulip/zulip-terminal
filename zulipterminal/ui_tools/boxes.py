785d04b65386237e5df42b9c90a100d1acb2f6f6
mubarak-mustopha <mustophamubarack17@gmail.com>
2026-05-09 15:20:02 +0100

    autocomplete: boxes: Prioritize users with recent DMs.
    Fixes #1526.
    Prioritize users with recent DMs after users in current topic then
    stream.

--- zulipterminal/ui_tools/boxes.py
+++ zulipterminal/ui_tools/boxes.py
@@ -565,18 +565,24 @@ class WriteBox(urwid.Pile):
         matching_ids = {user["user_id"] for user in matching_users}
         matching_recipient_ids = set(self.recipient_user_ids) & set(matching_ids)
 
-        # get user ids in the topic narrow
         if self.compose_box_status == "open_with_stream":
+            # get user ids in the topic narrow
             topic_narrow_user_ids = self.model.get_user_ids_in_topic_narrow(
                 self.stream_id, self.title_write_box.edit_text
             )
             matching_narrow_user_ids = set(topic_narrow_user_ids) & set(matching_ids)
-            # Display users in topic narrow first
+
+            # user ids with recent DMs
+            recent_dm_user_ids = self.model.get_user_ids_in_recent_pms()
+            matching_dm_user_ids = set(recent_dm_user_ids) & set(matching_ids)
+
+            # sort priority level:  topic narrow > stream > recent DMs
             sorted_matching_users = sorted(
                 matching_users,
                 key=lambda user: (
                     user["user_id"] in matching_narrow_user_ids,
                     user["user_id"] in matching_recipient_ids,
+                    user["user_id"] in matching_dm_user_ids,
                 ),
                 reverse=True,
             )
